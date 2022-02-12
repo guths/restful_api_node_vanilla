c6d99941fdb9568bf15bd25aaf3b375d247e6bdd
guths <jonathanguths@gmail.com>
2022-02-12 10:01:10 -0300

    primary file for the api

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log('teste')
+//Primary file for the api
+
+//Dependencies
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.end('Hello world !\n');
+});
+
+server.listen(3000, function () {
+    console.log('The server is listening on port 3000 now...')
+})
+
+
+
